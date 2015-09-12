73483f2811311f2c60d87c2e204c726c563ae71f
Jonathan Wainwright <jon@waindigo.org>
2015-09-12 18:21:38 +0100

    Beta 2

--- library/Cake/Messages/Install/FileHealthCheck.php
+++ library/Cake/Messages/Install/FileHealthCheck.php
@@ -7,7 +7,7 @@ class Install_FileHealthCheck extends \Cake\Install_FileHealthCheckBase
     public function getFileHashes()
     {
         return array(
-        	'library/Cake/Messages/addon-Cake_Messages.xml' => '1d771e3d4f745cd6bd7bf1c1807e3b02',
+        	'library/Cake/Messages/addon-Cake_Messages.xml' => 'b260123d67cd5a52a9aa0d8cb046010d',
         	'library/Cake/Messages/Install/Controller.php' => '3f233ef15ed6a47db9e800407abf755b',
         	'library/Cake/Messages/Install/Data.php' => '00d0d9721bdc210b4ed0cff4bb414cb7',
         	'js/cake/messages/index.html' => 'd41d8cd98f00b204e9800998ecf8427e',
